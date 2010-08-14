1aae00002728fcb59caf5a17ef5e6ee6b88cf646
Luke Lee <durdenmisc@gmail.com>
2010-08-14 10:59:07 -0500

    Getting rid of app urls b/c its confusing and the entire project is only going to consist of 1 app ... so far

--- spygitapp/urls.py
+++
@@ -1,5 +0,0 @@
-from django.conf.urls.defaults import *
-
-urlpatterns = patterns('django.views.generic.simple',
-    (r'^$', 'direct_to_template', {'template': 'home.html'}),
-)
--- urls.py
+++ urls.py
@@ -5,12 +5,13 @@ from django.conf import settings
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = patterns('django.views.generic.simple',
     # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
     # to INSTALLED_APPS to enable admin documentation:
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    (r'^$', include('spygitapp.urls')),
+    (r'^$', 'direct_to_template', {'template': 'home.html'}),
+    (r'^about/$', 'direct_to_template', {'template': 'about.html'}),
 
     # Uncomment the next line to enable the admin:
     (r'^admin/', include(admin.site.urls)),
