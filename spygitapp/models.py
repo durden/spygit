0db5424c255363f8ee176ecef366d4a034f42f87
Luke Lee <durdenmisc@gmail.com>
2010-08-14 01:37:04 -0500

    Added app to installed_apps and fixed syntax issues in models

--- spygitapp/models.py
+++ spygitapp/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 
 
-class Errors(models):
+class Errors(models.Model):
     error_type = models.IntegerField()
     short_descr = models.TextField()
     long_descr = models.TextField()
 
 
-class Runs(models):
+class Runs(models.Model):
     project_name = models.CharField(max_length=300)
     project_url = models.CharField(max_length=500)
     date = models.DateTimeField(auto_now_add=True)
@@ -15,6 +15,6 @@ class Runs(models):
     git_revision = models.CharField(max_length=40)
 
 
-class Files(models):
+class Files(models.Model):
     filename = models.CharField(max_length=300)
     run = models.ForeignKey('Runs')
