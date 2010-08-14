b617b841fc9afab1ed115b06ec6066652ddcc914
Luke Lee <durdenmisc@gmail.com>
2010-08-14 01:17:53 -0500

    Initial attempt at db setup/models

--- spygitapp/models.py
+++ spygitapp/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Errors(models):
+    error_type = models.IntegerField()
+    short_descr = models.TextField()
+    long_descr = models.TextField()
+
+
+class Runs(models):
+    project_name = models.CharField(max_length=300)
+    project_url = models.CharField(max_length=500)
+    date = models.DateTimeField(auto_now_add=True)
+    twitter_user = models.CharField(max_length=300)
+    git_revision = models.CharField(max_length=40)
+
+
+class Files(models):
+    filename = models.CharField(max_length=300)
+    run = models.ForeignKey('Runs')
